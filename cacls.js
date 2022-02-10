7e6ce167605ba050341d70ce34c8d8fd8ccb3448
Salman4800 <hasmanisalman10@gmail.com>
2022-02-10 08:15:55 +0530

    Calculator

---
+++ cacls.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    console.log(a+b);
+}
+
+function sub(x,y){
+    console.log(x-y);
+}
+
+function mul(x,y){
+    console.log(x*y);
+}
+function div(x,y){
+    console.log(x/y)
+}
+module.exports.addition = add
+module.exports.sub = sub
+module.exports.multiplication = mul  
+module.exports.division = div  
---
+++ index.js
@@ -0,0 +1,7 @@
+const calcs = require("./cacls") //import 
+ 
+calcs.addition(12,12)
+calcs.sub(12,12) 
+calcs.multiplication(12,12)
+calcs.division(24,12) 
+
